69be0d76a673273c1ac7df94dcc7194921aa1e97
bernkaztel <bernkaztel@hotmail.com>
2018-04-17 12:08:18 -0500

    subiendo componente home, finder navbar y funcionalidad router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home'
+import Finder from './components/finder'
+import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from "react-router-dom";
+
+
 
 class App extends Component {
+  constructor(){
+    super()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/finder" exact component={Finder}/>
+      </Switch>
       </div>
     );
   }
---
+++ src/components/finder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class finder extends Component {
+  render() {
+    return (
+      <div className="finder">
+       Este es el finder
+      </div>
+    )
+  }
+}
